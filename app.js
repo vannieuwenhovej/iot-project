d44b9e2cddc13c1312b58e741e180303463bee51
vannieuwenhovej <basketjonathan3@hotmail.com>
2019-12-16 21:26:43 +0100

    Full big implementation of working backend NodeJS API

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 var express = require("express");
 var app = express();
+
+var mysql = require('mysql');
+
+
+var connection = mysql.createConnection({
+    host     : 'aa1tl0a0trwpa2k.cicqvgam2cqn.us-west-2.rds.amazonaws.com',
+    port      :  3306,
+    user     : 'awsroot',
+    password : 'awsrootpass',
+    database : 'aa1tl0a0trwpa2k'
+
+});
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
+
+    //Connection Sample
+    connection.connect(function(err) {
+        if (err) {
+            console.error('Database connection failed: ' + err.stack);
+            return;
+        }
+
+        console.log('Connected to database.');
+    });
+
+    connection.end();
+
 });
 
 app.get("/", (req, res, next) => {
